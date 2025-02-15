f6e6d6cfaed988effd5ed04cd28a298959d2344d
CPS50 <124213704+CPS50@users.noreply.github.com>
2025-02-15 18:21:38 -0500

    Update - Use Our fruit_chosen Variable in the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ if ingredients_list:
     # To display smoothefroot nutrition information
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response.json()) #Expose/Show the JSON Data Inside the Response Object
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) #display JSON data in a Dataframe
 
